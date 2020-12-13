867c865238c0a626f26d3978ae262fa4877febbe
Rubertoss <71005087+Rubertoss@users.noreply.github.com>
2020-12-13 23:06:32 +0200

    Create user.js

---
+++ src/js/models/user.js
@@ -0,0 +1,32 @@
+const connection = require('../connection');
+const Sequilize = require('sequelize');
+
+class User extends Sequilize.Model {}
+
+User.init(
+    {
+        email: {
+            type: Sequilize.STRING(100),
+            allowNull: false
+        },
+        password: {
+            type: Sequilize.STRING(100),
+            allowNull: false
+        },
+        name: {
+            type: Sequilize.STRING(50),
+            allowNull: false
+        },
+        status: {
+            type: Sequilize.STRING(100),
+            allowNull: false
+        },
+    },
+    {
+        sequelize: connection,
+        freezeTableName: true,
+        modelName: 'User'
+    }
+);
+
+module.exports = User
